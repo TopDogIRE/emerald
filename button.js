2c9e5d281dc77368f1bc1a7f504c9337751c3131
TopDogIRE <cathal777@gmail.com>
2021-01-13 14:09:41 -0800

    Corrected

--- button.js
+++ button.js
@@ -2,11 +2,9 @@
 getDogos().catch(err => console.error(err));
 async function getDogos(){
     var response = await fetch('https://dog.ceo/api/breeds/image/random');
-    var blob = await response.blob();
+    var blob = await response.json();
 
     // create <img> for it
-    var img = document.createElement('img');
-    document.getElementById("redSetter").innerHTML = "<img src =\'" + URL.createObjectURL(blob) + "\'>";
+    document.getElementById("redSetter").innerHTML = "<img src =\'" + blob.message + "\'>";
 }
-// I have made it as far as the json file is represnted in the DOM as an image...
-// represntation but not as an actual image
+// Corrected the error
