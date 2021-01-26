ba4833a1fd04376f1c1732a677983d8b56651019
TopDogIRE <60242692+TopDogIRE@users.noreply.github.com>
2021-01-26 14:55:05 -0800

    Removing whitespace

--- httpServerNode.js
+++ httpServerNode.js
@@ -7,34 +7,27 @@ const users = [                 // Users info
     {user:'Rebecca', age:20},
     {user:'Indika', age:27}, 
 ];
- 
+// Deal with request from page
 const requestHandler = function(req, res){
-
     if (req.method === 'GET' && req.url == '/users') {          // Handles Get from /users
         res.writeHead(200);
         res.write(`<html><body>${JSON.stringify(users)}</body></html>`);
         res.end();
-
     }else if (req.method === 'POST' && req.url == '/') {        // Handles Post
         res.writeHead(200);
         let chunks = "";        // Initialize String 
-
         req.on('data', (chunk) => {
             chunks += chunk;
         });
-
         req.on('end', () => {
             const jsonData = JSON.parse(chunks);
             const namesOfJson = jsonData.user; // Getting the name form the array of objects
             res.write(`<h1>${nameOfJson}<h1>`);
             res.end();
         });
-        
     }else{
         res.end('Welecome to the page');        // Welcoming on the home page
     }
-
 }
-
 const server = http.createServer(requestHandler);
 server.listen(8080);            // http://localhost:8080/users
