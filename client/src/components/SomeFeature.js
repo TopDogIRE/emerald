38c33a538166be3dab5cff2c76253ef1fde6a997
TopDogIRE <60242692+TopDogIRE@users.noreply.github.com>
2021-01-26 15:01:07 -0800

    indentation correction

--- client/src/components/SomeFeature.js
+++ client/src/components/SomeFeature.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function SomeFeature(props){
     return (
-        <div className="container">
-        <h2>Some Feature that Cathal has added</h2>
-      </div>
-        )
+            <div className="container">
+                <h2>Some Feature that Cathal has added</h2> // To be displayed on DOM
+            </div>
+           )
 }
-
